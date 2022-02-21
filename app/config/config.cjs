df4fe57b981f299cb9c222b695eead97ed8903d2
gelugu <mkraev@vtb.ru>
2022-02-21 19:56:01 +0300

    revert solution

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,19 +1,24 @@
 require('dotenv').config();
 
-const postgresEnvConfig = {
-  dialect: 'postgres',
-  database: process.env.DATABASE_NAME,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.DATABASE_PORT,
-  host: process.env.DATABASE_HOST,
-};
-
 module.exports = {
   development: {
     dialect: 'sqlite',
     storage: './database.sqlite',
   },
-  production: postgresEnvConfig,
-  test: postgresEnvConfig,
+  production: {
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+  },
+  test: {
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+  },
 };
